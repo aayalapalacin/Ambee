e3a76218e7e9a4dcef03040b90471cf024bc4490
Alex <alexander.ayala.palacin@gmail.com>
2022-11-27 18:11:20 -0500

    created components folder, implemented App.Registry in App.js, tested button and card component from react native paper ui component library

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { AppRegistry } from "react-native";
+import MyButton from "./components/button";
+import MyCard from "./components/card";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your application</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your application</Text>
+        <MyButton />
+        <MyCard />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
-
+AppRegistry.registerComponent(App, () => Main);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
