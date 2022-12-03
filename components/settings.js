7fef34deeb74bf4f4e1ac3bfe3a140c460c36903
Alex <alexander.ayala.palacin@gmail.com>
2022-12-02 20:39:31 -0500

    got toggle icon working in settings.js

--- components/settings.js
+++ components/settings.js
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Checkbox, Switch } from "react-native-paper";
 import React from "react";
 
 const Settings = () => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   return (
     <View>
-      <Text>Settings</Text>
+      <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+      <Checkbox.Item label="Item" status="checked" />
     </View>
   );
 };
