3b2a3269c52c1acf41519d28e4cca645c982dba6
Alex <alexander.ayala.palacin@gmail.com>
2022-12-11 18:11:37 -0500

    finished what-type-of -movies screen

---
+++ components/optionBtn.js
@@ -0,0 +1,108 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Card, Title, Paragraph } from "react-native-paper";
+
+import React from "react";
+
+const genres = [
+  { name: "horror", key: 1 },
+  { name: "comedy", key: 2 },
+  { name: "doc", key: 3 },
+  { name: "sci-fi", key: 4 },
+  { name: "anime", key: 5 },
+  { name: "musical", key: 6 },
+  { name: "action", key: 7 },
+  { name: "romantic", key: 8 },
+];
+const OptionBtn = () => {
+  return (
+    <View>
+      <View style={styles.genreContainer}>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[0].name}</Text>
+        </Button>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[1].name}</Text>
+        </Button>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[2].name}</Text>
+        </Button>
+      </View>
+      <View style={styles.genreContainer}>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[3].name}</Text>
+        </Button>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[4].name}</Text>
+        </Button>
+      </View>
+      <View style={styles.genreContainer}>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[5].name}</Text>
+        </Button>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[6].name}</Text>
+        </Button>
+        <Button
+          style={styles.genreDiv}
+          compact={true}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={styles.genreTxt}>{genres[7].name}</Text>
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default OptionBtn;
+
+const styles = StyleSheet.create({
+  genreContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  genreDiv: {
+    width: 100,
+    marginVertical: 18,
+    marginHorizontal: 9,
+  },
+  genreTxt: {
+    fontSize: 19,
+  },
+});
--- components/options.js
+++ components/options.js
@@ -1,20 +1,61 @@
 import * as React from "react";
-import { Button } from "react-native-paper";
+import { Button, Card, Title, Paragraph } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+  FlatList,
+} from "react-native";
+import OptionBtn from "./optionBtn";
 
-const genres = ["horror", "comedy", "documentary", "sci-fi", "anime"];
-
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 const Options = () => {
-  return genres.map((genre, index) => (
-    <Button
-      key={index}
-      icon=""
-      compact={true}
-      mode="contained"
-      onPress={() => console.log("Pressed")}
-    >
-      {genre}
-    </Button>
-  ));
-};
+  const window = useWindowDimensions();
 
+  return (
+    <Card height={window.height} style={styles.container}>
+      <Card.Content>
+        <Title style={styles.cardTitle}>What type of movies do you like?</Title>
+        <OptionBtn />
+      </Card.Content>
+      <Button style={styles.continueBtn} mode="contained">
+        <Text style={styles.btnTxt}>Continue</Text>
+      </Button>
+    </Card>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 0,
+    marginHorizontal: 5,
+  },
+  cardTitle: {
+    textAlign: "center",
+    marginHorizontal: 10,
+    marginTop: 60,
+    marginBottom: 50,
+    fontFamily: "Mali-Bold",
+    fontSize: 32,
+  },
+
+  continueBtn: {
+    marginHorizontal: 90,
+    marginTop: 70,
+  },
+  btnTxt: {
+    fontFamily: "Mali-Regular",
+    fontSize: 20,
+  },
+  genreContainer: {
+    flexDirection: "row",
+  },
+  genreDiv: {
+    width: 100,
+  },
+});
 export default Options;
